dbf2151939131e0b492a69bc8b89330b628b9479
Takuya Ogata <ogata.t@tam-tam.co.jp>
2023-11-23 13:04:06 +0900

    テンプレートファイルの作成

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,5 @@ from .views import ArticleListview, ArticleCreateview
 
 urlpatterns = [
     path("", ArticleListview.as_view(), name="articles.list"),
-    path("create/", ArticleCreateview.as_view(), name="articles.list"),
+    path("create/", ArticleCreateview.as_view(), name="articles.create"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
-class ArticleListview(TemplateView):
+class ArticleListview(ListView):
     template_name = "articles/list.html"
+    model = Article
 
-class ArticleCreateview(TemplateView):
+class ArticleCreateview(CreateView):
     template_name = "articles/create.html"
+    model = Article
+    form_class = ArticleForm
+    success_url = "/articles/"
